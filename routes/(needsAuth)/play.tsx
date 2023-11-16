51828ad393920772b8a752446839eb3fd561995d
EthanSuperior <evan.k.chase@gmail.com>
2023-11-15 19:18:22 -0700

    Added username to play screen

--- routes/(needsAuth)/play.tsx
+++ routes/(needsAuth)/play.tsx
@@ -1,9 +1,11 @@
+import { getCookies } from "$std/http/cookie.ts";
+import { getUserByToken } from "../../database/database.tsx";
 import Log from "../../islands/Log.tsx";
 import OtrioGame from "../../islands/Otrio.tsx";
 import PlaySettings from "../../islands/Settings.tsx";
-export default function PlayOtrio(req:Request) {
-    const url = new URL(req.url);
-    const username = url.searchParams.get("q") ?? '';
+export default async function PlayOtrio(req:Request) {
+    const { authToken } = getCookies(req.headers);
+    const { username } = (await getUserByToken(authToken)) as unknown as {username:string};
 
 	//container padding: 0px 15px 0px 15px
     return (
